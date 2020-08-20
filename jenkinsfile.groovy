3cce17e44ac24b56f35ce7917ebbc1e779c4b3bc
moshirosang <tiw_tisadee@hotmail.com>
2020-08-20 10:21:30 +0700

    add jenkins file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,14 +1,35 @@
 pipeline {
-    agent {
-        docker {
-            image 'maven:3-alpine'
-            args '-v /root/.m2:/root/.m2'
-        }
-    }
+
+    agent any
+
     stages {
-        stage('Build') {
+
+        stage('Build Package file jar'){
             steps {
-                sh 'mvn -B -DskipTests clean package'
+                echo 'Build Package file jar'
+                echo '******************************'
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Upload jar file on Nexus server') {
+            steps {
+                echo 'Deploy on Nexus server'
+                echo '******************************'
+            }
+        }
+
+        stage('Deploy for test sit uat') {
+            steps {
+                echo 'Deploy for test sit uat'
+                echo '******************************'
+            }
+        }
+
+        stage('Start app on server') {
+            steps{
+                echo 'Start app'
+                echo '******************************'
             }
         }
     }
