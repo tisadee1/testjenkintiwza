da0c87a3b4700cdec35fd4e2d2887595e1ba653a
moshirosang <tiw_tisadee@hotmail.com>
2020-08-20 09:59:04 +0700

    first commint

---
+++ src/main/java/com/tiw/za/tiwza/TiwzaApplication.java
@@ -0,0 +1,13 @@
+package com.tiw.za.tiwza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiwzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiwzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiw/za/tiwza/TiwzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiw.za.tiwza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiwzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
